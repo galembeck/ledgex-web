b5e832d5d7627291a008129583054caf408490e2
Pedro Galembeck <galembeckpedro@gmail.com>
2026-03-18 17:34:23 -0300

    build: create initial project solution (w/ libraries & dependencies)

---
+++ src/pages/__root.tsx
@@ -0,0 +1,15 @@
+import { createRootRoute, HeadContent, Outlet } from "@tanstack/react-router";
+import { ThemeProvider } from "@/providers/theme-provider";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+});
+
+function RootComponent() {
+  return (
+    <ThemeProvider defaultTheme="light" storageKey="ledgex-theme">
+      <HeadContent />
+      <Outlet />
+    </ThemeProvider>
+  );
+}
---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import path from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+      generatedRouteTree: "./src/route-tree-gen.ts",
+      routesDirectory: "./src/pages",
+      routeToken: "layout",
+      quoteStyle: "double",
+    }),
+    react(),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(import.meta.dirname, "./src"),
+    },
+  },
+});
